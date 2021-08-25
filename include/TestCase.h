fe2cbf213251641ac217f8a9986a2ab3a728e347
Vasilev Artem <vasilev.artem@phystech.edu>
2021-08-25 15:40:56 +0300

    At least it works

--- include/TestCase.h
+++ include/TestCase.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include <gtest/gtest>
+#include "gtest/gtest.h"
 
 class TestCase : public ::testing::Test
 {
 public:
-	static void SetUpTestCase();
-	void SetUp() override;
-	void TearDown() override;
+	static void SetUpTestCase(){}
+	void SetUp() override {}
+	void TearDown() override {}
 };
